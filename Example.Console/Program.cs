3524b5f543caa72c1710ea4ec315392b14e27d1c
Brandon Russell <BRussell@ebsco.com>
2018-06-12 16:50:07 -0500

    * Added example project * Added cacheing to service

---
+++ Example.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ServiceModel;
+using FeatureFlagsService;
+
+namespace Example.Console
+{
+    class Program
+    {
+        public static RSFT.Core.FeatureFlagService FeatureFlagService { get; set; }
+
+        private static int CacheTime = 15;
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Initializing FeatureFlag Service.");
+
+            FeatureFlagService =
+                new RSFT.Core.FeatureFlagService(
+                    () => new FeatureRepositoryClient(RSFT.Core.FeatureFlagService.GetBindingForEndpoint(), 
+                        new EndpointAddress("http://featureflags.boss.ebsco.com/Service/CentralizedFeatureRepository.svc")), 
+                    "PROD", "EPOP", CacheTime);
+
+            FeatureFlagService.Initialize();
+
+            System.Console.WriteLine("Testing some flags...");
+            WriteValuesOut();
+            System.Console.WriteLine($"Sleeping cache time of {CacheTime}");
+            System.Threading.Thread.Sleep(new TimeSpan(0,0,0,CacheTime));
+            WriteValuesOut();
+
+            System.Console.ReadKey();
+        }
+
+        static void WriteValuesOut()
+        {
+            System.Console.WriteLine("---------------------------");
+
+            foreach (var item in FeatureFlagService.Features)
+            {
+                System.Console.WriteLine($"{item.Name} PROD Value: {FeatureFlagService.IsAvailable(item.Name)}");
+            }
+
+            System.Console.WriteLine("---------------------------");
+        }
+    }
+}
--- RSFT.Core/FeatureFlagService.cs
+++ RSFT.Core/FeatureFlagService.cs
@@ -8,31 +8,41 @@ namespace RSFT.Core
     {
         private readonly Func<IFeatureRepository> _featureFlagFactoryFunc;
 
-        private TimeSpan _lastUpdateTime;
-		
-		public string Tenant { get; private set; }
+        private DateTime _lastUpdateTime;
+        
+        public string Tenant { get; private set; }
 
-		public string FeaturePrefix { get; private set; }
+        public string FeaturePrefix { get; private set; }
 
-		public int CacheTime { get; private set; }
+        public int CacheTime { get; private set; }
 
-		public Feature[] Features { get; private set; }
+        public Feature[] Features { get; private set; }
 
         private readonly object _lockObject = new object();
 
-        public FeatureFlagService(Func<IFeatureRepository> featureFlagFactoryFunc, TimeSpan lastUpdateTime, string tenant, string featurePrefix, int cacheTime)
+        public FeatureFlagService(Func<IFeatureRepository> featureFlagFactoryFunc, 
+            string tenant, string featurePrefix, int cacheTime)
         {
             _featureFlagFactoryFunc = featureFlagFactoryFunc;
-            _lastUpdateTime = lastUpdateTime;
             Tenant = tenant;
             FeaturePrefix = featurePrefix;
             CacheTime = cacheTime;
-            _lastUpdateTime = TimeSpan.MinValue;
+            _lastUpdateTime = DateTime.MinValue;
+        }
+
+        public static System.ServiceModel.Channels.Binding GetBindingForEndpoint()
+        {
+            System.ServiceModel.BasicHttpBinding result = new System.ServiceModel.BasicHttpBinding();
+            result.MaxBufferSize = int.MaxValue;
+            result.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+            result.MaxReceivedMessageSize = int.MaxValue;
+            result.AllowCookies = true;
+            return result;
         }
 
         private void RefreshCache()
         {
-            if (_lastUpdateTime.Seconds <= CacheTime && Features != null)
+            if (DateTime.Now.Subtract(_lastUpdateTime).TotalSeconds <= CacheTime && Features != null)
             {
                 return;
             }
@@ -46,16 +56,22 @@ namespace RSFT.Core
             lock (_lockObject)
             {
                 Features = result;
+                _lastUpdateTime = DateTime.Now;
             }
+        }
+
+        public void Initialize()
+        {
+            RefreshCache();
         }
 
         public bool IsAvailable(string featureName)
         {
-			// Refresh our cache if needed
-			RefreshCache();
+            // Refresh our cache if needed
+            RefreshCache();
 
             var result = Features
-				.FirstOrDefault(f => f.Name.Equals(featureName))
+                .FirstOrDefault(f => f.Name.Equals(featureName))
                 ?.SupportedTenants
                 .Any(t => t.Equals(Tenant));
 
